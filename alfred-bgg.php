0bce9ca1048be85c19dd8ef3ad8a192e891452eb
zendy <zendywongso@gmail.com>
2016-10-31 11:18:24 +1100

    Use query as input

--- alfred-bgg.php
+++ alfred-bgg.php
@@ -1,9 +1,14 @@
 <?php
+  $query = $argv[1];
+  $query = strtolower( trim( $query ) );
+
+  $bgg_url = "https://www.boardgamegeek.com/xmlapi2/search?query=" . urlencode( $query );
+
   // create curl resource
   $ch = curl_init();
 
   // set url
-  curl_setopt($ch, CURLOPT_URL, "https://www.boardgamegeek.com/xmlapi2/search?query=blood+rage");
+  curl_setopt($ch, CURLOPT_URL, $bgg_url);
 
   //return the transfer as a string
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -17,18 +22,13 @@
   // $games = simplexml_load_file( "simple.xml" );
   $games = simplexml_load_string( $result );
 
-  // foreach ($games as $game):
-  //   $title = $game->name[0]['value'];
-  //   echo $title;
-  //   echo "<br />";
-  // endforeach;
-
   $items = new SimpleXMLElement("<items></items>"); 	// Create new XML element
 
   foreach ($games as $game):
-    $title = $game->name[0]['value'];
+    $title = htmlspecialchars( $game->name[0]['value'] );
     $c = $items->addChild( 'item' );
     $d = $c->addChild( 'title', $title );
+    // $d = $c->addChild( 'title', urlencode( $query ) );
   endforeach;
 
   echo $items->asXML();

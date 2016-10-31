040de7ac48b9298c4bdcca52c3254bbe9f99b0a9
zendy <zendywongso@gmail.com>
2016-10-31 19:56:54 +1100

    Add more information on result

--- alfred-bgg.php
+++ alfred-bgg.php
@@ -37,25 +37,65 @@
     return $ids;
   }
 
+  function getNumberOfPlayers( $min, $max ){
+    $stringNumberOfPlayers = "";
+
+    if ( $min > $max) {
+      $stringNumberOfPlayers = $min . " players";
+    } elseif ( $min == $max ) {
+      if( $min == 0 ) {
+        $stringNumberOfPlayers = "Not Set";
+      } else {
+        $stringNumberOfPlayers = $max . "+" . $min . " players";
+      }
+    } else {
+      $stringNumberOfPlayers = $min . "-" . $max . " players";
+    }
+
+    return $stringNumberOfPlayers;
+  }
+
+  function getPlayingTime( $playingtime ){
+    $stringPlayingTime = "";
+    if ( $playingtime == 0) {
+      $stringPlayingTime = "Not Set";
+    } else {
+      $stringPlayingTime = $playingtime . " minutes";
+    }
+
+    return $stringPlayingTime;
+  }
+
   function constructXMLResult( $games ){
     $items = new SimpleXMLElement("<items></items>"); 	// Create new XML element
 
     foreach ($games as $game):
       $title = htmlspecialchars( $game->name[0]['value'] );
-      $subtitle = substr( htmlspecialchars( $game->description ), 0, 50 );
+      $description = htmlspecialchars( $game->description );
+      $avg_rating = (float)htmlspecialchars( $game->statistics->ratings->average[0]['value'] );
+      $geek_rating = (float)htmlspecialchars( $game->statistics->ratings->bayesaverage[0]['value'] );
+      $year_published = htmlspecialchars( $game->yearpublished[0]['value'] );
+      $min_players = (int)htmlspecialchars( $game->minplayers[0]['value'] );
+      $max_players = (int)htmlspecialchars( $game->maxplayers[0]['value'] );
+      $playing_time = (int)htmlspecialchars( $game->playingtime[0]['value'] );
+      $rank = htmlspecialchars( $game->statistics->ratings->ranks->rank[0]['value'] );
+      $weight = htmlspecialchars( $game->averageweight[0]['value'] );
       $thumb = htmlspecialchars( $game->thumbnail );
+
+      $avg_rating = round( $avg_rating, 2 );
+      $geek_rating = round( $geek_rating, 2 );
+      $combinedTitle = $title;
+      $combinedDescription =  $year_published . " | rating: " . $avg_rating . " | rank: " . $rank . " | " . getNumberOfPlayers( $min_players, $max_players ) . " | " . getPlayingTime( $playing_time );
+
       $c = $items->addChild( 'item' );
-      $d = $c->addChild( 'title', $title);
-      $e = $c->addChild( 'subtitle', $subtitle );
-      $f = $c->addChild( 'icon', "http:" . $thumb );
-      $f->addAttribute( 'type', 'fileicon' );
+      $d = $c->addChild( 'title', $combinedTitle);
+      $e = $c->addChild( 'subtitle', $combinedDescription );
     endforeach;
 
     return $items;
   }
 
-  // $query = $argv[1];
-  $query = "suburbia";
+  $query = $argv[1];
   $query = urlencode( strtolower( trim( $query ) ) );
 
   $resultSearch = callAPI( searchURL( $query ) );
